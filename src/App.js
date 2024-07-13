06f029232e2239c60fe4ab6713e1df4754362a4b
CoderNamedNick <153879485+CoderNamedNick@users.noreply.github.com>
2024-07-13 10:15:31 -0400

    made header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import Intro from "./intro";
+import Header from "./header";
+import Projects from "./Projects";
 
 const App = () => {
   return (
     <div className="App">
+      <Header/>
       <Intro/>
-      <div className="Projects-div">
-
-      </div>
+      <Projects/>
     </div>
   );
 }
---
+++ src/header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+
+  return(
+    <div className="header">
+      <div>Intro</div>
+      <div>Projects</div>
+      <div>Contacts</div>
+    </div>
+  )
+}
+
+export default Header
