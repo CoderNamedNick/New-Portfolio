c0651f39ea07ca38bf420441e2f4decb6af41a8c
CoderNamedNick <153879485+CoderNamedNick@users.noreply.github.com>
2024-07-17 09:13:17 -0400

    finished new portfolio

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # New-Portfolio
- old sucks so i make a new
+ My New portfolio feel free to Fork the Repo an make it you if you like the design or something. Go see it live at 
--- src/header.js
+++ src/header.js
@@ -6,13 +6,13 @@ const Header = () => {
   return (
     <div className="header">
       <Link to="intro" smooth={true} duration={500}>
-        <div>Intro</div>
+        <div style={{cursor: 'pointer'}}>Intro</div>
       </Link>
       <Link to="projects" smooth={true} duration={500}>
-        <div>Projects</div>
+        <div style={{cursor: 'pointer'}}>Projects</div>
       </Link>
       <Link to="contacts" smooth={true} duration={500}>
-        <div>Contacts</div>
+        <div style={{cursor: 'pointer'}}>Contacts</div>
       </Link>
     </div>
   );
